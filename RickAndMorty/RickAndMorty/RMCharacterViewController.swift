2d9e541efb6ccc33a4a64e67076852476bd20166
parutech <paruvitu@gmail.com>
2023-09-23 16:30:52 -0300

    feat: CharactersListViewViewModel

--- RickAndMorty/RickAndMorty/RMCharacterViewController.swift
+++ RickAndMorty/RickAndMorty/RMCharacterViewController.swift
@@ -16,12 +16,17 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        setupView()
+           
+    }
+    
+    private func setupView() {
         view.addSubview(characterListView)
         NSLayoutConstraint.activate ([
             characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
         characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
         characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
         characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-            ])
+        ])
     }
 }
