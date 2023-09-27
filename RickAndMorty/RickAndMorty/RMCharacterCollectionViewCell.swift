ce1e0ff460a15828216c1a873253e80a577a74f6
parutech <paruvitu@gmail.com>
2023-09-26 21:16:03 -0300

    feat: RMCharacterCollectionViewCell

--- RickAndMorty/RickAndMorty/RMCharacterCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/RMCharacterCollectionViewCell.swift
@@ -11,9 +11,37 @@ import UIKit
 class RMCharacterCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+        
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubviews(ImageView, nameLabel, statusLabel)
+        addConstraint()
     }
     
     required init?(coder: NSCoder) {
@@ -21,12 +49,17 @@ class RMCharacterCollectionViewCell: UICollectionViewCell {
     }
     
     private func addConstraint() {
+        NSLayoutConstraint.activate([
         
+        ])
     }
     
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
+        nameLabel.text = nil
+        statusLabel.text = nil
     }
     
     
