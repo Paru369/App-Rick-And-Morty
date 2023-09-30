2fcfa1893edad444c9ae71192c86545f16318f5a
parutech <paruvitu@gmail.com>
2023-09-30 15:00:48 -0300

    add RMCharacterDetailView

--- RickAndMorty/RickAndMorty/RMCharacterDetailViewController.swift
+++ RickAndMorty/RickAndMorty/RMCharacterDetailViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
+/// Controller to detail of characters
+///
 class RMCharacterDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override init(viewModel: ) {
+        super.init(nibName: nil, bundle: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init? (coder: NSCoder) {
+        fatalError("Unsupported")
     }
-    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        
+    }
 
 }
