2f9d181d9f4b4bf5af15d1de0416c0360c6c923a
parutech <paruvitu@gmail.com>
2023-10-07 21:08:37 -0300

    feat: RMCharacterPhotoCollectionViewCell

--- RickAndMorty/RickAndMorty/RMCharacterInfoCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/RMCharacterInfoCollectionViewCell.swift
@@ -9,11 +9,48 @@ import UIKit
 
 final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifer = "RMCharacterInfoCollectionViewCell"
+   
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Earth"
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Location"
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "earth")
+        return icon
+    }()
+    
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
+    // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.addSubviews(
+            titleContainerView,
+            valueLabel,
+            iconImageView
+        )
+        titleContainerView.addSubview(titleLabel)
+        setUpConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -21,11 +58,15 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setUpConstraints() {
-        
-    }
+        NSLayoutConstraint.activate([
+            
+        ])    }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        valueLabel.text = nil
+        titleLabel.text = nil
+        iconImageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
