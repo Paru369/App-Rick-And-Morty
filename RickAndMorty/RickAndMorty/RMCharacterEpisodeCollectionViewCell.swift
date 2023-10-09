4e797029675cab48d372f694e6589a3a8b984df0
parutech <paruvitu@gmail.com>
2023-10-09 13:32:56 -0300

    feat: RMCharacterEpisodeCollectionViewCell

--- RickAndMorty/RickAndMorty/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/RMCharacterEpisodeCollectionViewCell.swift
@@ -10,10 +10,36 @@ import UIKit
 final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     static let cellIdentifer = "RMCharacterEpisodeCollectionViewCell"
     
+    private let seasonLabel: UILabel = {
+        let label = UILabel()
+  
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let airDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .tertiarySystemBackground
+        contentView.backgroundColor = .systemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.systemBlue.cgColor
+        contentView.addSubviews(seasonLabel, nameLabel, airDateLabel)
     }
     
     required init?(coder: NSCoder) {
