a049949faa1bae7d8b9e7c7ab8148dcbebc29c90
parutech <paruvitu@gmail.com>
2023-10-10 14:00:24 -0300

    feat: RMEpisodeDetailViewViewModel

--- RickAndMorty/RickAndMorty/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/RickAndMorty/RMEpisodeDetailViewViewModel.swift
@@ -17,6 +17,20 @@ class RMEpisodeDetailViewViewModel {
     }
 
     private func fetchEpisodeData() {
+        guard let url = endpointUrl,
+                let request = RMRequest(url: url) else {
+            return
+        }
         
+        RMService.shared.execute(request,
+                                 expecting: RMEpisode.self) { result in
+            
+            switch result {
+            case .success(let success):
+                print(String(describing: success))
+            case .failure(let failure):
+                print(String(describing: failure))
+            }
+        }
     }
 }
