1c0a856a015dd380b2fe48dab716777326577418
parutech <paruvitu@gmail.com>
2023-10-12 21:14:24 -0300

    feat: RMSearchViewController

--- RickAndMorty/RickAndMorty/RMSearchViewController.swift
+++ RickAndMorty/RickAndMorty/RMSearchViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class RMSearchViewController: UIViewController {
 
+    
+    struct Config {
+        enum `Type` {
+            case character
+            case episode
+            case location
+        }
+        
+        let type: `Type`
+    }
+    private let config: Config
+    
+    init(config: Config) {
+        self.config = config
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Search"
 
      
     }
