e31b383c5ab208c68b6f48a175774e2f2d0a1cbc
parutech <paruvitu@gmail.com>
2023-10-23 23:13:19 -0300

    feat: RMEpisodeDetailViewController

--- RickAndMorty/RickAndMorty/RMEpisodeDetailViewController.swift
+++ RickAndMorty/RickAndMorty/RMEpisodeDetailViewController.swift
@@ -7,53 +7,67 @@
 
 import UIKit
 
-
 /// VC to show details about single episode
-class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
-  
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate, RMEpisodeDetailViewDelegate {
     private let viewModel: RMEpisodeDetailViewViewModel
+
     private let detailView = RMEpisodeDetailView()
-    
+
     // MARK: - Init
+
     init(url: URL?) {
         self.viewModel = RMEpisodeDetailViewViewModel(endpointUrl: url)
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError()
     }
-    
-    
-    //MARK: - LifeCycle
-    
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubviews(detailView)
-        viewModel.delegate = self
+        view.backgroundColor = .systemBackground
+        view.addSubview(detailView)
         addConstraints()
+        detailView.delegate = self
         title = "Episode"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+
+        viewModel.delegate = self
+        viewModel.fetchEpisodeData()
     }
-    
+
     private func addConstraints() {
         NSLayoutConstraint.activate([
-        detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-        detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-        detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-        detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
-    
+
     @objc
     private func didTapShare() {
-        
+
     }
-    
-    //MARK: - Delegate
-    
+
+    // MARK: - View Delegate
+
+    func rmEpisodeDetailView(
+        _ detailView: RMEpisodeDetailView,
+        didSelect character: RMCharacter
+    ) {
+        let vc = RMCharacterDetailViewController(viewModel: .init(character: character))
+        vc.title = character.name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    // MARK: - ViewModel Delegate
+
     func didFetchEpisodeDetails() {
         detailView.configure(with: viewModel)
     }
-
 }
