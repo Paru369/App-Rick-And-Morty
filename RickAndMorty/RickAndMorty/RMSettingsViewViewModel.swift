0520a21daac9b9f9f2fe1ad24b813f024accffe6
parutech <paruvitu@gmail.com>
2023-10-24 22:45:24 -0300

    feat: RMSettingsViewViewModel

---
+++ RickAndMorty/RickAndMorty/RMSettingsViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Paulo Pinheiro on 10/24/23.
+//
+
+import Foundation
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
