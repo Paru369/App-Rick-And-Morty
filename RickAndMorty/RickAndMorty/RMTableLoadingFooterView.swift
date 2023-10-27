297c427b876a434ebdbb4b726f02d5e6ef0d3ff4
parutech <paruvitu@gmail.com>
2023-10-27 09:20:20 -0300

    feat: RMTableLoadingFooterView

---
+++ RickAndMorty/RickAndMorty/RMTableLoadingFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by Paulo Pinheiro on 10/27/23.
+//
+
+import UIKit
+
+class RMTableLoadingFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
