c1a0892b63a4da8ff6c51990ab3a2728097ef8a3
parutech <paruvitu@gmail.com>
2023-10-27 20:54:28 -0300

    feat: RMNoSearchResultsViewViewModel

---
+++ RickAndMorty/RickAndMorty/RMNoSearchResultsViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMNoSearchResultsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Paulo Pinheiro on 10/27/23.
+//
+
+
+import UIKit
+
+struct RMNoSearchResultsViewViewModel {
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
+}
