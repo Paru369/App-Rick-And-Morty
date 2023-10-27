92def728f2e6bd95f03f634fc7e0a5ebe492613d
parutech <paruvitu@gmail.com>
2023-10-27 09:20:36 -0300

    feat: RMTableLoadingFooterView

--- RickAndMorty/RickAndMorty/RMTableLoadingFooterView.swift
+++ RickAndMorty/RickAndMorty/RMTableLoadingFooterView.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class RMTableLoadingFooterView: UIView {
+final class RMTableLoadingFooterView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(spinner)
+        spinner.startAnimating()
+
+        addConstraints()
     }
-    */
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+        ])
+    }
 
 }
